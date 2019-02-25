c5fd76ed5654fbc0f2aca048b37347a35b472ce3
TakushiYokoyama <takushi.yokoyama@jointcrew.co.jp>
2019-02-25 18:16:34 +0900

    ゴミを削除

--- lesson2/src/app.tsx
+++ lesson2/src/app.tsx
@@ -1,42 +1,8 @@
-import React, { Component } from 'react';
-import { ColorProperty } from 'csstype';
-import { getStep } from './service';
+import * as React from 'react';
 
-interface ColoredParagraphProps {
-  color: ColorProperty;
-}
-const ColoredParagraph1: React.SFC<ColoredParagraphProps> = props => {
-  const { color } = props;
-  const [step, setStep] = React.useState<number | undefined>(undefined);
-  const asyncFunc = async () => {
-    setStep(await getStep());
-  };
-  React.useEffect(() => {
-    asyncFunc();
-  }, []);
-  return <p style={{ color }}>{step}</p>;
-};
-class ColoredParagraph2 extends React.Component<ColoredParagraphProps> {
-  constructor(props: any) {
-    super(props);
-  }
-  public async componentDidMount() {
-    const step = await getStep();
-    this.setState({ step });
-  }
+class App extends React.Component {
   public render() {
-    const { color, children } = this.props;
-    return <p style={{ color }}>{children}</p>;
-  }
-}
-class App extends Component {
-  public render() {
-    return (
-      <div className="App">
-        <ColoredParagraph1 color="red">red</ColoredParagraph1>
-        <ColoredParagraph2 color="blue">blue</ColoredParagraph2>
-      </div>
-    );
+    return <div>xxx</div>;
   }
 }
 
